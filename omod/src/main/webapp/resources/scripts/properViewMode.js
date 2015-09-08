7c857efccf97e85cf73c5b7bc97d79c352f2c2b6
Romain Buisson <romain@mekomsolutions.com>
2015-09-08 14:44:42 +0700

    Adding the properViewModeForTable() function Improve properViewMode() to better display obs 	modified:   omod/src/main/webapp/resources/scripts/properViewMode.js

--- omod/src/main/webapp/resources/scripts/properViewMode.js
+++ omod/src/main/webapp/resources/scripts/properViewMode.js
@@ -2,63 +2,117 @@
 // Generic JavaScript to proper the VIEW mode (handles the toggleContainers, the "when" HFE markup and empty observations)
 
 
+
 var properViewMode = function (formUuid) { 
 
 	jQuery("htmlform").each(function (index, currentForm) {
 		if (jQuery(currentForm).attr("formUuid") == formUuid) {
 
-			// Handle the VIEW mode for the toggleContainers
-			// display only fields with observations
+			/* Handle the VIEW mode for the toggleContainers */
+			/* display only fields with observations */
 			jQuery(currentForm).find(".toggleContainer").each(function (index, toggleContainer) {
 				if (!(jQuery(toggleContainer).find('.value').text() == "")) {
 					jQuery(toggleContainer).show();
 				}
 			});
 
-			// remove buttons
+			/* remove buttons */
 			jQuery(currentForm).find('.addEntry').remove();
 			jQuery(currentForm).find('.removeEntry').remove();
 
 			var i = 1;
+			
 
-			jQuery(currentForm).find("fieldset").each(function (index) {
+			jQuery(currentForm).find("section").each(function (indexS, currentSection) {
 
-				var hideFieldset = false;
+				var emptyFieldset = -1;
+				var hideSection = false;
 
-				jQuery(this).hide();
+				jQuery(currentSection).find("fieldset").each(function (indexF, currentFieldset) {
 
-				// Show all non-empty obs
-				jQuery(this).find(".value").closest("fieldset").show();
+					var hideFieldset = false;
 
-				// Hide empty values
-				var emptyValue = -1;
-				jQuery(this).find(".value").each(function (index, element) {
-					// Hide "p" section when value is empty
-					if (jQuery(element).text() == "") {
-						jQuery(element).closest("p").hide();
-						emptyValue= emptyValue+1;
-					}	
-					if ((emptyValue == index)) {
-						hideFieldset = true;
+					jQuery(currentFieldset).hide();
+
+					/* Show all non-empty obs */	
+					jQuery(currentFieldset).find(".value").closest("fieldset").show();
+
+					/* Hide empty values */
+					var emptyValue = -1;
+					jQuery(currentFieldset).find(".value").each(function (indexV, element) {
+						/* Hide "p" section when value is empty */
+						if (jQuery(element).text() == "") {
+							jQuery(element).closest("p").hide();
+							emptyValue= emptyValue+1;
+						}	
+						if ((emptyValue == indexV)) {
+							hideFieldset = true;
+						} else {
+							hideFieldset = false;
+						}
+					});
+
+					/* hide the "p" section when Obs have emptyValue */
+					jQuery(currentFieldset).find(".emptyValue").closest("p").hide();
+
+					if (hideFieldset) {
+						jQuery(currentFieldset).hide()
+					}
+
+					/* handle the "when" markup */
+					jQuery(currentFieldset).find(".thenDisplay").find(".value").each(function (indexD, thenDisplay) {
+						if (jQuery(thenDisplay).text() == "") {
+							jQuery(thenDisplay).closest("p").hide();
+						}
+					});
+					i = i+1;
+
+					if ($(currentFieldset).css("display") == "none"){
+						emptyFieldset = emptyFieldset+1;
+					}
+
+					if(emptyFieldset == indexF) {
+						hideSection = true;
+					} else {
+						hideSection = false;
 					}
 				});
+if (hideSection) {
+	jQuery(currentSection).hide()
+}
 
-				// hide the "p" section when Obs have emptyValue
-				jQuery(this).find(".emptyValue").closest("p").hide();
 
+});
+}
+});
+}
 
-				if (hideFieldset) {
-					jQuery(this).hide()
+var properViewModeForTable = function  (formUuid) {
+
+	jQuery("htmlform").each(function (index, currentForm) {
+		if (jQuery(currentForm).attr("formUuid") == formUuid) {
+
+			jQuery(currentForm).find("fieldset").find("td").each(function (index, td){
+				if (jQuery(td).find(".emptyValue").text()) {
+					jQuery(td).hide();
 				}
-
-				// handle the "when" markup
-				jQuery(this).find(".thenDisplay").find(".value").each(function (index, thenDisplay) {
-					if (jQuery(this).text() == "") {
-						jQuery(this).closest("p").hide();
-					}
-				});
-				i = i+1;
 			});
-		}
-	});
+
+					// replacing the tables by a list of "p" included in a "span"
+					$("table").each(function(index, table){
+						var span = $("<span>")
+						$("th", this).each(function() {
+							var title = $("<span>");
+							title.append(this.innerHTML);
+							$(title).addClass("title");
+							span.append(title);
+						});
+						$("td", this).each(function(){
+							span.append(this.innerHTML.replace("[X]&nbsp;", ""));
+						});
+						$(table).replaceWith(span);
+					})
+
+				}
+			});
 }
