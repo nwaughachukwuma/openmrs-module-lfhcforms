53608a11480b106476557a4b095f8a1059665ac8
Dimitri Renault <dimitri@mekomsolutions.com>
2015-09-20 12:30:21 +0200

    Forms's XML is configurable to add their meta data. Javadoc added and final changes. 	modified:   api/src/main/java/org/openmrs/module/lfhcforms/activator/HtmlFormsInitializer.java 	modified:   api/src/main/java/org/openmrs/module/lfhcforms/utils/ExtensionFormUtil.java <htmlform> attributes added for 	modified:   omod/src/main/webapp/resources/htmlforms/clinicalnotes.html 	modified:   omod/src/main/webapp/resources/htmlforms/diagnosis.html 	modified:   omod/src/main/webapp/resources/htmlforms/diagnostic-imaging.html 	modified:   omod/src/main/webapp/resources/htmlforms/discharge.html 	modified:   omod/src/main/webapp/resources/htmlforms/dispense-med.html 	modified:   omod/src/main/webapp/resources/htmlforms/fluidbalance.html 	modified:   omod/src/main/webapp/resources/htmlforms/history.html 	modified:   omod/src/main/webapp/resources/htmlforms/immunity.html 	modified:   omod/src/main/webapp/resources/htmlforms/ipd-nurse.html 	modified:   omod/src/main/webapp/resources/htmlforms/lab-test-order.html 	modified:   omod/src/main/webapp/resources/htmlforms/lines-tubes.html 	modified:   omod/src/main/webapp/resources/htmlforms/med-order.html 	modified:   omod/src/main/webapp/resources/htmlforms/opd-nurse.html 	modified:   omod/src/main/webapp/resources/htmlforms/phys-exam.html 	modified:   omod/src/main/webapp/resources/htmlforms/treatment.html 	modified:   omod/src/main/webapp/resources/htmlforms/triage.html 	modified:   omod/src/main/webapp/resources/htmlforms/vitals-pews.html 	modified:   omod/src/main/webapp/resources/htmlforms/wound.html

--- api/src/main/java/org/openmrs/module/lfhcforms/activator/HtmlFormsInitializer.java
+++ api/src/main/java/org/openmrs/module/lfhcforms/activator/HtmlFormsInitializer.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.Form;
 import org.openmrs.api.FormService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.formentryapp.FormEntryAppService;
@@ -20,10 +19,12 @@ import org.openmrs.module.lfhcforms.LFHCFormsActivator;
 import org.openmrs.module.lfhcforms.utils.ExtensionFormUtil;
 import org.openmrs.ui.framework.resource.ResourceFactory;
 import org.openmrs.ui.framework.resource.ResourceProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Sets up the HFE forms
+ * 1) Scans the webapp/resources/htmlforms folder
+ * 2) Attempts to create an HFE form from each of the files
+ * 3) Adds the forms as in Configure Metadata \ Manage Forms
  */
 public class HtmlFormsInitializer implements Initializer {
 
@@ -42,6 +43,7 @@ public class HtmlFormsInitializer implements Initializer {
 		final ResourceFactory resourceFactory = ResourceFactory.getInstance();
 		final ResourceProvider resourceProvider = resourceFactory.getResourceProviders().get(providerName);
 		
+		// Scanning the forms resources folder
 		final List<String> formPaths = new ArrayList<String>();
 		final File formsDir = resourceProvider.getResource(formsPath); // The ResourceFactory can't return File instances, hence the ResourceProvider need
 		if(formsDir == null || formsDir.isDirectory() == false) {
@@ -49,25 +51,31 @@ public class HtmlFormsInitializer implements Initializer {
 			return;
 		}
 		for(File file : formsDir.listFiles()) 
-			formPaths.add(formsPath + file.getName());
+			formPaths.add(formsPath + file.getName());	// Adding each file's path to the list
 		
+		// Save form + add its meta data
 		final FormManager formManager = Context.getRegisteredComponent("formManager", FormManager.class);
 		final FormEntryAppService hfeAppService = Context.getRegisteredComponent("formEntryAppService", FormEntryAppService.class);
 		final FormService formService = Context.getFormService();
 		final HtmlFormEntryService hfeService = Context.getService(HtmlFormEntryService.class);
 		for (String formPath : formPaths) {
+			// Save form
 			HtmlForm htmlForm = null;
 			try {
 				htmlForm = HtmlFormUtil.getHtmlFormFromUiResource(resourceFactory, formService, hfeService, providerName, formPath);
 			} catch (IOException e) {
 				log.error("Could not generate HTML form from the following resource file: " + formPath, e);
 				continue;
+			} catch (IllegalArgumentException e) {
+				log.error("Error while parsing the form's XML: " + formPath, e);
+				continue;
 			}
+			// Adds meta data
 			ExtensionForm extensionForm = null;
 			try {
 				extensionForm = ExtensionFormUtil.getExtensionFormFromUIResourceAndForm(resourceFactory, providerName, formPath, hfeAppService, formManager, htmlForm.getForm());
 			} catch (Exception e) {
-				log.error("The form was created but its extension point could not be created in Configure Metadata: " + formPath, e);
+				log.error("The form was created but its extension point could not be created in Manage Forms \\ Configure Metadata: " + formPath, e);
 				continue;
 			}
 		}
--- api/src/main/java/org/openmrs/module/lfhcforms/utils/ExtensionFormUtil.java
+++ api/src/main/java/org/openmrs/module/lfhcforms/utils/ExtensionFormUtil.java
@@ -1,6 +1,8 @@
 package org.openmrs.module.lfhcforms.utils;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.jfree.util.Log;
 import org.openmrs.Form;
@@ -13,21 +15,45 @@ import org.openmrs.ui.framework.resource.ResourceFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
+/**
+ * Equivalent of HtmlFormUtil for ExtensionForm objects.
+ * 
+ * @author Dimitri Renault
+ *
+ */
 public class ExtensionFormUtil {
-
+	
+	public static final String DEFAULT_UILOCATION = "patientDashboard.overallActions";
+	
+	/**
+	 * Returns the ExtensionForm corresponding to a Form instance AND saves it.
+	 * @param providerName Eg. "lfhcforms".
+	 * @param formPath The relative form path under webapp/resources (eg. "htmlforms/myform.xml")
+	 * @param form The Form instance
+	 */
 	public static ExtensionForm getExtensionFormFromUIResourceAndForm(ResourceFactory resourceFactory, String providerName, String formPath, FormEntryAppService hfeAppService, FormManager formManager, Form form) throws Exception {
 
-		String xml = resourceFactory.getResourceAsString(providerName, formPath);
+		final String xml = resourceFactory.getResourceAsString(providerName, formPath);
+		
 		final ExtensionForm extensionForm = getExtensionFormFromXML(xml);
+		extensionForm.setId("");
+		extensionForm.setLabel(form.getName());
 		
-		if(extensionForm != null) {
+		// If this form is already saved on the specified UI location, we remove it first
+		final List<Extension> extensions = hfeAppService.getFormExtensions(form);
+		for(Extension extension : extensions) {
+			final ExtensionForm runningExtensionForm = new ExtensionForm();
+			runningExtensionForm.copyFrom(extension);
+			if(runningExtensionForm.getUiLocation().equals(extensionForm.getUiLocation())) {
+				hfeAppService.purgeFormExtension(form, extension);
+				break;
+			}
+		}
 		
-			extensionForm.setId("");
-			extensionForm.setLabel(form.getName());
-			
-			// If this form is already hooked to an extension, we don't do anything.
-			final List<Extension> extensions = hfeAppService.getFormExtensions(form);
-			if(extensions.isEmpty()) {
+		// Then if the provided UI location is valid, we add it to that one
+		final Set<String> allowedUiiLocations = new HashSet<String>( formManager.getUILocations(form) );
+		if(allowedUiiLocations.contains(extensionForm.getUiLocation())) {
+			if(extensionForm.getOrder() != null) {	// This must be satisfied when the XML has been parsed (a bit clunky)
 				final Extension extension = new Extension();
 				extensionForm.copyTo(extension);
 				hfeAppService.saveFormExtension(form, extension);
@@ -37,9 +63,12 @@ public class ExtensionFormUtil {
 		return extensionForm;
 	}
 	
-	public static ExtensionForm getExtensionFormFromXML(String xml) throws Exception {
+	/**
+	 * @return A <i>partially</i> filled ExtensionForm from the XML representation of a form.
+	 */
+	protected static ExtensionForm getExtensionFormFromXML(String formXlm) throws Exception {
 		
-		Document doc = HtmlFormEntryUtil.stringToDocument(xml);
+		Document doc = HtmlFormEntryUtil.stringToDocument(formXlm);
         Node htmlFormNode = HtmlFormEntryUtil.findChild(doc, "htmlform");
 
         String processFlag = getAttributeStringValue(htmlFormNode, "formAddMetadata", "");
@@ -48,7 +77,8 @@ public class ExtensionFormUtil {
 
         final ExtensionForm extensionForm = new ExtensionForm();
         if(doProcess) {
-        	extensionForm.setOrder( getAttributeIntegerValue(htmlFormNode, "formIcon", 1) );
+        	extensionForm.setOrder( getAttributeIntegerValue(htmlFormNode, "formOrder", 0) );
+        	extensionForm.setUiLocation( getAttributeStringValue(htmlFormNode, "formUILocation", DEFAULT_UILOCATION) );
         	extensionForm.setIcon( getAttributeStringValue(htmlFormNode, "formIcon", "icon-file") );
         	extensionForm.setDisplayStyle( getAttributeStringValue(htmlFormNode, "formDisplayStyle", "Simple") );
 	        extensionForm.setShowIf( getAttributeStringValue(htmlFormNode, "formShowIf", "") );
@@ -57,12 +87,12 @@ public class ExtensionFormUtil {
         return extensionForm;
 	}
 	
-    private static String getAttributeStringValue(Node htmlForm, String attributeName, String defaultValue) {
+    protected static String getAttributeStringValue(Node htmlForm, String attributeName, String defaultValue) {
         Node item = htmlForm.getAttributes().getNamedItem(attributeName);
         return item == null ? defaultValue : item.getNodeValue();
     }
     
-    private static int getAttributeIntegerValue(Node htmlForm, String attributeName, int defaultValue) {
+    protected static int getAttributeIntegerValue(Node htmlForm, String attributeName, int defaultValue) {
     	String stringValue = getAttributeStringValue(htmlForm, attributeName, (new Integer(defaultValue)).toString());
     	int val = defaultValue;
     	try {
