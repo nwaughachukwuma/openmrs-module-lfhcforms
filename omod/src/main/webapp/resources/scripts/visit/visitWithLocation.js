1cf954778fd24cb8fb79be6e1c2b91668e404b8f
Romain <ro.buisson@gmail.com>
2015-12-05 15:50:56 +0100

    LFHCDESK-42: Disable the current 'Start Visit' button displayed in the visits lists dashboard 	modified:   omod/src/main/webapp/resources/scripts/visit/visitWithLocation.js

--- omod/src/main/webapp/resources/scripts/visit/visitWithLocation.js
+++ omod/src/main/webapp/resources/scripts/visit/visitWithLocation.js
@@ -1,26 +1,19 @@
 var visit = visit || {};
 
-visit.reloadPageWithoutVisitId = function() {
-    window.location.search = window.location.search.replace(/visitId=[^\&]*/g, "");
-}
-
-
 visit.startVisitWithLocationDialog = null;
 
-visit.createStartVisitWithLocationDialog = function(patientId) {
+visit.createStartVisitWithLocationDialog = function() {
 	visit.startVisitWithLocationDialog = emr.setupConfirmationDialog({
 		selector: '#start-visit-with-location-dialog',
 		actions: {
-			confirm: function() {
+			confirm: function() {	
 				emr.getFragmentActionWithCallback('lfhcforms', 'visit/visitWithLocation', 'create',
 					{ patientId: visit.patientId,
 						selectedLocation: jq('#visit-location-drop-down').find(":selected").val() },
-					function(data) {
-						jq('#start-visit-with-location-dialog' + ' .icon-spin').css('display', 'inline-block').parent().addClass('disabled')
-						// debugger;
+						function(data) {
+							jq('#start-visit-with-location-dialog' + ' .icon-spin').css('display', 'inline-block').parent().addClass('disabled');
 						visit.reloadPageWithoutVisitId();
 					},function(err){
-						// debugger;
 						visit.reloadPageWithoutVisitId();
 					});
 			},
@@ -38,7 +31,32 @@ visit.showStartVisitWithLocationDialog = function(patientId) {
 	//console.log(patientId);
 	if (visit.startVisitWithLocationDialog == null) {
 		visit.createStartVisitWithLocationDialog();
-	}
+	};
 	visit.startVisitWithLocationDialog.show();
 };
 
+
+$(document).ready(
+	// override the existing 'Start Visit' button to trigger the new 'StartVisitWithLocation' dialog instead of extisting action.
+	// this function is not called by an extension point (different from showStartVisitWithLocationDialog() ).
+	// Therefore, we need to retrieve the Patient ID from the URL parameters.
+	function replaceStartVisitButton() {
+	if (document.getElementById("noVisitShowVisitCreationDialog") != null) {
+		$("#noVisitShowVisitCreationDialog").attr("href","");
+		var classString = document.getElementById('noVisitShowVisitCreationDialog').className;
+		var label = document.getElementById('noVisitShowVisitCreationDialog').text;
+		$("#noVisitShowVisitCreationDialog").replaceWith('<span id="noVisitShowVisitCreationDialog" class="' + classString + '">' + label + '</span>' );
+		$("#noVisitShowVisitCreationDialog").click(function () {
+			var patientId = getParameterByName("patientId");
+			visit.showStartVisitWithLocationDialog(patientId);
+		});
+	}
+})
+
+// retrieve patientId from URL.
+function getParameterByName(name) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+	results = regex.exec(location.search);
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
