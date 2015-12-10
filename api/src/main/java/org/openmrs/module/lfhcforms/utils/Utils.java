904ffdb4a0aedf106b2baefb4e4f6c4da0a436e7
Romain <ro.buisson@gmail.com>
2015-12-10 13:45:48 +0100

    LFHCDESK-42: Only Transfer encounter is created when visit type is changed 	modified:   api/src/main/java/org/openmrs/module/lfhcforms/utils/Utils.java

--- api/src/main/java/org/openmrs/module/lfhcforms/utils/Utils.java
+++ api/src/main/java/org/openmrs/module/lfhcforms/utils/Utils.java
@@ -8,8 +8,6 @@ import java.util.Map;
 import java.util.NavigableMap;
 import java.util.TreeMap;
 
-import javax.mail.Session;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Encounter;
@@ -17,19 +15,14 @@ import org.openmrs.EncounterType;
 import org.openmrs.Location;
 import org.openmrs.LocationAttribute;
 import org.openmrs.LocationAttributeType;
-import org.openmrs.LocationTag;
 import org.openmrs.Patient;
 import org.openmrs.Person;
 import org.openmrs.Visit;
 import org.openmrs.VisitType;
-import org.openmrs.api.AdministrationService;
 import org.openmrs.api.EncounterService;
-import org.openmrs.api.LocationService;
 import org.openmrs.api.VisitService;
-import org.openmrs.module.appframework.context.SessionContext;
-import org.openmrs.module.emrapi.EmrApiProperties;
-import org.openmrs.module.emrapi.visit.VisitDomainWrapper;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.emrapi.visit.VisitDomainWrapper;
 import org.openmrs.module.lfhcforms.LFHCFormsConstants;
 
 /**
@@ -72,53 +65,42 @@ public class Utils {
 	 * 
 	 * @param visit
 	 */
-	public static void setAdmissionBasedOnVisitType(Visit visit, Location loginLocation) {
-		setAdmissionBasedOnVisitType(visit, loginLocation, null);
+	public static void setEncounterBasedOnVisitType(Visit visit, Location loginLocation) {
+		setEncounterBasedOnVisitType(visit, loginLocation, null);
 	}
 
 	
 	
 	/**
-	 * Sets admission status based on visit location
+	 * Create encounters based on visit type
 	 * 
 	 * @param visit
-	 * @param previousLocation
+	 * @param loginLocation
+	 * @param previousType
 	 */
-	public static void setAdmissionBasedOnVisitType(Visit visit, Location loginLocation, VisitType previousType) {
+	public static void setEncounterBasedOnVisitType(Visit visit, Location loginLocation, VisitType previousType) {
 
-		// anytime a visit type is changed (visit type != previousType): create a transfer encounter.
-		// if visit type is Inpatient and previousType is null, then create an Admission encounter, instead of Transfer
+		// any time a visit type is changed (visit type != previousType): create a transfer encounter.
 
 		// all types are transfer type
 		boolean isTransferType = true;
-		boolean isAdmissionType = false;
 
 		if (visit.getVisitType() == previousType) {
+			// visit type is not changed: do nothing
 			return;
 		}
-
-		if (visit.getVisitType().getUuid().equals(LFHCFormsConstants.INPATIENT_VISIT_TYPE_UUID) ) {
-			// isAdmissionType
-			isAdmissionType = true;
-		}
-
-		// retrieve admission status
-		EmrApiProperties emrApiProperties = Context.getRegisteredComponent("emrApiProperties", EmrApiProperties.class);
-		VisitDomainWrapper visitWrapper = new VisitDomainWrapper(visit, emrApiProperties);
-		boolean isAdmitted = visitWrapper.isAdmitted();
 
 		EncounterService es = Context.getEncounterService();
 		VisitService vs = Context.getVisitService();
 		Person person = Context.getUserContext().getAuthenticatedUser().getPerson();
 
-		setAdmission(visit, vs, es, person, loginLocation, previousType, isAdmitted, isAdmissionType, isTransferType);
+		setEncounter(visit, vs, es, person, loginLocation, previousType, isTransferType);
 
 	}
 
 	/**
 	 * 
-	 * Create admission, discharge or transfer encounters for visits, based on
-	 * visit location and current admission status
+	 * Creates a transfer encounter when visit type is changed
 	 * 
 	 * @param visit
 	 * @param vs
@@ -126,36 +108,19 @@ public class Utils {
 	 * @param person
 	 * @param loginLocation
 	 * @param previousType
-	 * @param isAdmitted
-	 * @param isAdmissionType
 	 * @param isTransferType
 	 * 
 	 */
-	protected static void setAdmission(Visit visit, VisitService vs, EncounterService es, Person person, Location loginLocation,
-			VisitType previousType, boolean isAdmitted, boolean isAdmissionType, boolean isTransferType) {
+	protected static void setEncounter(Visit visit, VisitService vs, EncounterService es, Person person, Location loginLocation,
+			VisitType previousType, boolean isTransferType) {
 
-		EncounterType admissionEncounterType = es
-				.getEncounterTypeByUuid(LFHCFormsConstants.ADMISSION_ENCOUNTER_TYPE_UUID);
-		EncounterType dischargeEncounterType = es
-				.getEncounterTypeByUuid(LFHCFormsConstants.DISCHARGE_ENCOUNTER_TYPE_UUID);
 		EncounterType transferEncounterType = es
 				.getEncounterTypeByUuid(LFHCFormsConstants.TRANSFER_ENCOUNTER_TYPE_UUID);
 
 		Patient patient = visit.getPatient();
 		Encounter encounter = new Encounter();
 
-		if (isAdmissionType && !isAdmitted && previousType == null) {
-			// add Admission encounter
-			encounter.setEncounterType(admissionEncounterType);
-			encounter.setPatient(patient);
-			encounter.setEncounterDatetime(new Date());
-			encounter.setProvider(person);
-			encounter.setLocation(loginLocation);
-			es.saveEncounter(encounter);
-			visit.addEncounter(encounter);
-		} else if ( (isAdmissionType && isTransferType && previousType != null)
-				|| (!isAdmissionType && isTransferType && !isAdmitted ) 
-				) {
+		if ( previousType != null && isTransferType) {
 			// add Transfer encounter
 			encounter.setEncounterType(transferEncounterType);
 			encounter.setPatient(patient);
@@ -164,19 +129,8 @@ public class Utils {
 			encounter.setLocation(loginLocation);
 			es.saveEncounter(encounter);
 			visit.addEncounter(encounter);
-		} else if (!isAdmissionType && isAdmitted) {
-			// add Discharge encounter
-			encounter.setEncounterType(dischargeEncounterType);
-			encounter.setPatient(patient);
-			encounter.setEncounterDatetime(new Date());
-			encounter.setProvider(person);
-			encounter.setLocation(loginLocation);
-			es.saveEncounter(encounter);
-			visit.addEncounter(encounter);
+			vs.saveVisit(visit);
 		}
-
-		vs.saveVisit(visit);
-
 	}
 
 	/**
@@ -237,7 +191,7 @@ public class Utils {
 	 * 
 	 * Returns a map of the color and short name for a given list of visits
 	 * 
-	 * @param recentVisits
+	 * @param visits
 	 * @return
 	 */
 	public static Map<Integer, Map<String, Object>> getVisitColorAndShortName(List<VisitDomainWrapper> visits) {
