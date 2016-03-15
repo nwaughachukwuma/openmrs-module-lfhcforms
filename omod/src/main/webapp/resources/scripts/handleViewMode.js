9eec872a81e16bde36ac86ed94931b55b04fdb62
Romain <ro.buisson@gmail.com>
2016-03-15 14:34:41 +0700

    LFHCFORMS-127: Improve the handleViewMode.js function
    	modified:   omod/src/main/webapp/resources/scripts/handleViewMode.js
    	modified:   omod/src/main/webapp/resources/styles/lfhcforms.css

--- omod/src/main/webapp/resources/scripts/handleViewMode.js
+++ omod/src/main/webapp/resources/scripts/handleViewMode.js
@@ -1,54 +1,11 @@
-	// By Romain - romain@mekomsolutions.com
-// Generic JavaScript to clean the VIEW mode (handles the toggleContainers, the "when" HFE markup and empty observations...)
-
-/* All forms should be in the form of :
-<htmlform formUuid="44305e02-7e49-4f5a-8acf-49042acbe483" formName="OPD Nurse" formEncounterType="c7700650-63a6-4893-976e-7a7a0cc43b04" formVersion="0.4.5">
-
-<ifMode mode="VIEW">
-	<script type="text/javascript" id="handleViewModeForTable">
-		// JavaScript for VIEW mode
-		$(document).ready(function(){
-			handleViewMode("44305e02-7e49-4f5a-8acf-49042acbe483");
-			handleViewModeForTable("44305e02-7e49-4f5a-8acf-49042acbe483");
-		});
-	</script>
-</ifMode>
-
-
-<div class="hidden" id="who-when-where">
-	...
-</div>
-
-<section id="general" sectionTag="section" headerStyle="title" headerCode="General">
-
-	<fieldset>
-		<legend>
-			Other Infectious Concerns
-		</legend>
-		<h3>
-			Other infectious concerns
-		</h3>
-		// observations should be nested in a <p>. One <obs> per <p>. Ideally, one <obs> per <fieldset>
-		<p>
-			<obs conceptId="CIEL:159395" rows="4" cols="50"/>
-		</p>
-	</fieldset>
-
-</section>
-<submit/>
-<htmlform>
-*/
-
-
-
 var handleViewMode = function (formUuid) { 
 
 	$("htmlform").each(function (index, currentForm) {
 		if ($(currentForm).attr("formuuid") == formUuid) {
+
 			
-			
-			/* Handle the VIEW mode for the toggleContainers */
-			/* display only fields with observations */
+
+			/* display only '.toggleContainers' fields with observations */
 			/* See toggleContainers.js for more documentation */
 			$(currentForm).find(".toggleContainer").each(function (index, toggleContainer) {
 				if (!($(toggleContainer).find('.value').text() == "")) {
@@ -60,8 +17,7 @@ var handleViewMode = function (formUuid) {
 			$(currentForm).find('.addEntry').remove();
 			$(currentForm).find('.removeEntry').remove();
 
-			var i = 1;
-			
+
 			/* Iterate through each section of the current HTML form */
 			$(currentForm).find("section").each(function (indexS, currentSection) {
 
@@ -70,165 +26,66 @@ var handleViewMode = function (formUuid) {
 
 				$(currentSection).find("fieldset").each(function (indexF, currentFieldset) {
 
-					$(currentFieldset).hide();
-
-					/* Show all non-empty obs */	
-					$(currentFieldset).find(".value").closest("fieldset").show();
-
-					/* Hide empty values */
-
-					
-					/* Hide a "p" if it has only empty values */
-					var emptySpan = -1;
-					var hideParagraph = false;
-
-					$(currentFieldset).find("span").each(function (indexS, span) {
-						if ($(span).hasClass(".emptyValue")) {
-							$(span).hide();
-							emptySpan = emptySpan + 1;
-						}
-
-						if (emptySpan == indexS) {
-							hideParagraph = true;
-						} else {
-							hideParagraph = false;
-						}
-
-						if (hideParagraph) {
-							$(span).closest("p").hide();
-						} else {
-							$(span).closest("p").show();				
-						}
-					})
-
-
-					var emptyValue = -1;
 					var hideFieldset = false;
 
+					if ($(currentFieldset).find("p").length > 1) {
+						$(currentFieldset).find("*").addClass("multiple");
+						$(currentFieldset).find("h3").addClass("indented");
+					}
+
+					/* Hide elements with class value and text null */
 					$(currentFieldset).find(".value").each(function (indexV, element) {
 						/* Hide "p" section when value is empty */
 						if ($(element).text() == "") {
-
 							$(element).closest("p").hide();
 							$(element).closest("p").prev("h3").hide();
-							emptyValue= emptyValue+1;
+							$(element).addClass("emptyValue");
+						}
+					});
+
+					/*  */
+					$(currentFieldset).find("p").prev("h3").addClass("questionLabel");
+
+					/* hide the parent "p" section when Obs have emptyValue */
+					$(currentFieldset).find(".emptyValue").closest("p").hide();
+					$(currentFieldset).find(".emptyValue").closest("p").prev("h3").hide();
+
+					var emptyValue = -1;
+					/* hide the fieldset when all "p" contain ".emptyValue" */
+					$(currentFieldset).find("p").each(function (indexP, currentP) {
+						if ($(currentP).find('.emptyValue').val() == "") {
+							emptyValue = emptyValue + 1;	
 						}
 
-						
-						if ((emptyValue == indexV)) {
+						if ((emptyValue == indexP)) {
 							hideFieldset = true;
 						} else {
 							hideFieldset = false;
 						}
-
-						/* display the text of a "p" as an obs title when there is class "questionLabel" */
-						/*<p>
-						/*		<span class="questionLabel">Patient is in contact of known or suspected infectious case</span>
-						/*		<obs conceptId="162633"/>
-						/*</p>
-						*/
-
-						$(element).closest("p").find(".questionLabel").filter(":visible").each(function (indexL, labelSpan) {
-
-							/* get the free text of the obs to be used as h3 */
-							var obsTitle = $(labelSpan)
-							.clone()
-							.children()
-							.remove()
-							.end()
-							.text();
-
-							/* get the innerHTML of the obs value */
-							var obsSpan = $(element).closest("span");
-							/* get the innerHTML of the obs units*/
-							var unitsSpan = $(element).next("span.units");
-
-							/* Create a new "p" */
-							var newP = $("<p>");
-							$(newP).append("<span class=\"obs-field\">"+$('<div>').append(obsSpan.clone()).html()+$('<div>').append(unitsSpan.clone()).html()+"</span>");
-							
-							if ($(labelSpan).closest("div").attr("class") == "order") {
-								var newSpan = $("<span>");
-								$(newSpan).append("<h3>"+obsTitle+" </h3>"+$('<div>').append($(newP).clone()).html());
-								/* replace the current "p" by its new version */
-								$(element).closest("p").replaceWith($('<div>').append($(newSpan).clone()).html());
-							} else {
-								/* replace the current "p" by its new version */
-								$(element).closest("p").replaceWith("<h3>"+obsTitle+" </h3>"+$('<div>').append($(newP).clone()).html());
-							}
-						});
-
 					});
-
-					
 
 					if (hideFieldset) {
-						$(currentFieldset).hide()
+						$(currentFieldset).addClass("emptyFieldset");
 					}
-
-					/* handle the "when" markup */
-					$(currentFieldset).find(".thenDisplay").find(".value").each(function (indexD, thenDisplay) {
-						if ($(thenDisplay).text() == "") {
-							$(thenDisplay).closest("p").hide();
-						}
-					});
-					i = i+1;
 
 					/* if all fieldsets of the current section are hidden, define an empty fieldset */
-					if ($(currentFieldset).css("display") == "none"){
-						emptyFieldset = emptyFieldset+1;
+					if ($(currentFieldset).find(".emptyFieldset").val() == ""){
+						emptyFieldset = emptyFieldset + 1;
 					}
-
-					/*1 if all fieldsets are empty, define the hideSection */
+					/* if all fieldsets are empty, define the hideSection */
 					if(emptyFieldset == indexF) {
 						hideSection = true;
 					} else {
 						hideSection = false;
 					}
+
 				});
 
 				if (hideSection) {
-					$(currentSection).hide()
+					$(currentSection).addClass("emptySection");
 				}
-
 
 			});
 		}
-	});
-}
-
-var handleViewModeForTable = function  (formUuid) {
-
-	$("htmlform").each(function (index, currentForm) {
-
-		if ($(currentForm).attr("formUuid") == formUuid) {
-
-			/* iterate through each <td> of the forms */
-			$(currentForm).find("fieldset").find("td").each(function (index, td){
-				/* remove if the <td> has emptyValue */
-				if ($(td).find(".emptyValue").text()) {
-					$(td).remove();
-				}
-			});
-
-			/* replacing the tables by a list of "p" included in a "span" */
-			$("table").each(function(index, table){
-				var span = $("<span>");
-				$(span).addClass("autogenerated");
-				$("th", this).each(function() {
-					var title = $("<span>");
-					title.append(this.innerHTML);
-					$(title).addClass("title");
-					span.append(title);
-				});
-				$("td", this).each(function(){
-					span.append(this.innerHTML);
-				});
-				$(table).replaceWith(span);
-			})
-
-		}
-
-	});
-
+	})
 }
