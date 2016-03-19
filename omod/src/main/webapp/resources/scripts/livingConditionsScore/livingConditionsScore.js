dcb8bf2bfbe8e8e95dfeb66ea28ff55195d51a75
Romain <ro.buisson@gmail.com>
2016-03-19 21:22:24 +0700

    LFHCFORMS-126: Add more tests and create function
    	modified:   omod/src/main/webapp/resources/scripts/livingConditionsScore/livingConditionsScore.js
    	new file:   omod/src/test/webapp/resources/lcsTest.json
    	deleted:    omod/src/test/webapp/resources/livingConditions.json
    	modified:   omod/src/test/webapp/resources/scripts/livingConditionsScoreTest.js

--- omod/src/main/webapp/resources/scripts/livingConditionsScore/livingConditionsScore.js
+++ omod/src/main/webapp/resources/scripts/livingConditionsScore/livingConditionsScore.js
@@ -2,24 +2,188 @@
 
 // TODO: Retrieve this map from external resource
 var json = {
-	"LFHC:1320": {
-		"0":">=4",
-		"1":"2-3",
-		"2":"0-1"
-	},
-	"LFHC:1321": {
-		"0":"LFHC:1400",
-		"1":"LFHC:1401",
-		"2":"LFHC:1402"
+	"name":"lcsCalculationTable",
+	"description":"Holds a list of each question that is taken into account while calucation the Living Condition score. Each question has then a scoreMap property that maps the possible answers with their associated score",
+	"questions": {
+		"LFHC:1000":{
+			"scoreMap": {
+				"0": {
+					"value":"7",
+					"operator":">="
+				},
+				"1":{
+					"low": "2",
+					"high":"6"
+				},
+				"2": {
+					"value":"1"
+				},
+				"3": {
+					"value": "0"
+				}
+			}
+		},
+		"LFHC:1001":{
+			"scoreMap": {
+				"0":"LFHC:1100",
+				"1":"LFHC:1101",
+				"2":"LFHC:1102"
+			}
+		}
 	}
 };
 
+var input = {
+	"LFHC:1000":"3",
+	"LFHC:1001":"LFHC:1102",
+}
 
-var calculateLCS = function (inputMap, jsonMap) {
-	for (var concept in inputMap) {
-		if (inputMap.hasOwnProperty(concept)) {
-			alert(key + " -> " + p[key]);
+
+var calculateLCS = function (input, jsonMap) {
+
+	var score = null;
+	var lcs = {};	
+
+
+	/* Work the input to create object */
+
+	var userInput = [];
+
+	for (var individualInput in input) {
+
+		var questionWithAnswer = {};
+		questionWithAnswer.question = individualInput;
+		questionWithAnswer.answer = input[individualInput];
+
+		if ((questionWithAnswer.answer.indexOf(":")) > 0) {
+			questionWithAnswer.isNumeric = false;
+		} else {
+			questionWithAnswer.isNumeric = true;
+		}
+
+		userInput.push(questionWithAnswer);
+	}
+
+
+	/* Compare input to provided map */
+
+	for (var input in userInput) {
+
+		var questionWithAnswer = userInput[input];
+		var hasConfig = false;
+
+		for (var question in jsonMap.questions ) {
+			
+			var scoreMap = jsonMap.questions[question].scoreMap;
+
+			if (questionWithAnswer.question == question) {
+				hasConfig = true;
+				var questionScore;
+				if (questionWithAnswer.isNumeric) {
+					questionScore = getScoreFromNumeric(questionWithAnswer.answer, scoreMap);
+				} else {
+					questionScore = getScoreFromConcept(questionWithAnswer.answer, scoreMap);
+				}
+				// console.log(question + ": " + questionScore);
+				if (questionScore) {
+					score = questionScore *1 + score;
+				} else {
+					// console.warn("Score value for answer " + questionWithAnswer.answer + " (question " + question + ") is not found in configuration file")
+					lcs.value = null;
+					lcs.error="1";
+					return lcs;
+				}
+			}
+		}
+
+		if (!hasConfig) {
+			// console.warn("question " + questionWithAnswer.question + " is not found in configuration file")
+			lcs.value = null;
+			lcs.error="2";
+			return lcs;
 		}
 	}
 
+	lcs.value = score;
+	lcs.error="0";
+	return lcs;
 }
+
+var getScoreFromNumeric = function (userAnswer, answersScore) {
+	//// console.log('is numeric');
+
+	var score = null;
+
+	for (var currentScore in answersScore){
+
+		var aS = answersScore[currentScore];
+		var match = false;
+		// console.log(aS);
+
+		switch (aS.operator) {
+
+			case "<=": 
+			if (userAnswer <= aS.value*1) {
+				// console.log("Score: "+ currentScore + " (" +userAnswer + aS.operator + aS.value + ")");
+				match = true;
+			}
+			break;
+
+			case ">=":
+			if (userAnswer >= aS.value*1) {
+				// console.log("Score: "+ currentScore + " (" +userAnswer + aS.operator + aS.value + ")");
+				match = true;
+			}
+			break;
+
+			case "<":
+			if (userAnswer < aS.value*1) {
+				// console.log("Score: "+ currentScore + " (" +userAnswer + aS.operator + aS.value + ")");
+				match = true;
+			}
+			break;
+
+			case ">":
+			if (userAnswer > aS.value*1) {
+				// console.log("Score: "+ currentScore + " (" +userAnswer + aS.operator + aS.value + ")");
+				match = true;
+			}
+			break;
+
+			default:
+			if (userAnswer == aS.value*1) {
+				// console.log("Score: "+ currentScore + " (" +userAnswer + "=" + aS.value + ")");
+				match = true;
+			}
+
+		}
+
+		if (aS.high && aS.low) {
+			if (userAnswer >= aS.low && userAnswer <= aS.high ) {
+				// console.log("Score: "+ currentScore + " (" +userAnswer + ' in [' + aS.low +"-"+ aS.high + "])");
+				match = true;
+			}
+
+		}
+
+		if (match) {
+			score = currentScore;
+		}
+	}
+	return score;
+}
+
+var getScoreFromConcept = function (userAnswer, answersScore) {
+	// console.log('is concept');
+	var score = null;
+	for (var currentScore in answersScore){
+		// console.log(answersScore[currentScore]);
+		
+		if (userAnswer === answersScore[currentScore]) {
+			score = currentScore;
+		}
+	} 
+	return score;
+}
+
+
--- omod/src/test/webapp/resources/scripts/livingConditionsScoreTest.js
+++ omod/src/test/webapp/resources/scripts/livingConditionsScoreTest.js
@@ -1,13 +1,55 @@
 describe("Living Conditions Score Calculation", function() {
 
-    it("Expect Livin Condition Score to be 3", function() {
-    	var input = {
-    		"LFHC:1320":"1",
-    		"LFHC:1321":"LFHC:1401"
-    	}
+	it("should do basic score calculation", function() {
+		var input = {
+			"LFHC:1000":"1",
+			"LFHC:1001":"LFHC:1101"
+		}
 
-    	var score = calculateLCS(input,"livingConditions.js");
+		var score = calculateLCS(input,json);
 
-    	expect(score).toBe(3);
-    });
+		expect(score.value).toBe(3);
+	});
+
+	it("should handle range ('high' and 'low' properties)", function() {
+		var input = {
+			"LFHC:1000":"5"
+		}
+
+		var score = calculateLCS(input,json);
+		expect(score.value).toBe(1);
+	});
+
+	it("should handle 'greater than' and 'lower than' operators", function() {
+		var input = {
+			"LFHC:1000":"10",
+			"LFHC:1000":"0"
+		}
+
+		var score = calculateLCS(input,json);
+		expect(score.value).toBe(3);
+	});
+
+
+	it("should return error code 1 when answer is not found", function() {
+		var input = {
+			"LFHC:1001":"LFHC:9999"
+		}
+
+		var score = calculateLCS(input,json);
+		expect(score.error).toBe('1');
+		expect(score.value).toBeNull;
+	});	
+
+	it("should return error code 2 when question is not found", function() {
+		var input = {
+			"LFHC:9999":"1"
+		}
+
+		var score = calculateLCS(input,json);
+
+		expect(score.error).toBe('2');
+		expect(score.value).toBeNull;
+	});	
+
 });
